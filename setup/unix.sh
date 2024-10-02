6a622b0b077b717143af44650b8ecc2aea97eb96
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-02 16:41:39 -0400

    Create unix.sh\

---
+++ setup/unix.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download
+cd ..
+echo Makking the main haxelib and setuping folder in same time..
+mkdir ~/haxelib && haxelib setup ~/haxelib
+echo Installing dependencies...
+echo This might take a few moments depending on your internet speed.
+haxelib install lime 8.0.0
+haxelib install openfl 9.3.3
+haxelib install flixel 4.11.0
+haxelib install flixel-addons 2.9.0
+haxelib install flixel-tools 1.5.1
+haxelib install hxcpp
+haxelib install hxCodec 1.4.0
+haxelib install hxdiscord_rpc 2.5.1
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+haxelib git linc_luajit https://github.com/ianharrigan/hscript-ex
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/ashea-code/faxe
+haxelib install actuate
+echo Finished!
